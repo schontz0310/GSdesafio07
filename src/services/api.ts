62a01cd2e73f83c49ec1967c6ed7227944c3de87
schontz0310 <schontz0310@gmail.com>
2020-08-02 20:16:14 -0300

    test finalized

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
+import { Platform } from 'react-native';
 import axios from 'axios';
 
+const url =
+  Platform.OS === 'ios' ? 'http://localhost:3333' : 'http://10.0.2.2:3333';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: url,
 });
 
 export default api;
